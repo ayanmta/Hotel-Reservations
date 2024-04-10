a375b82a75fe49809146df7f9a5ab044b28c4e16
Ayan Mehta <ayanmehta@Ayans-Mac-mini.local>
2024-04-10 14:24:53 +0530

    new changes minor tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -7,28 +7,45 @@ const Navbar = () => {
   const handleToggle = () => {
     setOpen(!isOpen);
   };
+  const menuRef = useRef(null)
+  const handleOutsideClick=(e)=>{
+if(menuRef.current && !menuRef.current.contains(e.target)){
+  setOpen(false)
+}
+  }
+  const handleMenuItemClick = () => {
+    setOpen(false);
+    // Additional logic for menu item click
+  };
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    // Clean up event listener when component unmounts
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <Link to="/">      <h3 className="small-logo" style={{color:"",fontWeight:'bold'}}>🇹🇸🇧</h3>
+          <Link to="/">      <h3 className="small-logo" style={{color:"",fontWeight:'bold'}}> <span style={{fontWeight:"lighter"}}>| </span>T <span style={{fontWeight:"lighter"}}>| </span>S <span style={{fontWeight:"lighter"}}>| </span>B <span style={{fontWeight:"lighter"}}>| </span></h3>
 </Link>
           <button type="button" className="nav-btn" onClick={handleToggle}>
             <FaAlignRight className="nav-icon" />
           </button>
         </div>
         <ul className={isOpen ? "nav-links show-nav" : "nav-links"}>
-         <li><li>
+         <li onClick={handleMenuItemClick} ref={menuRef}><li>
             <Link to="/">Home</Link>
           </li>
           <span className="specialNav">
-          <li>
+          <li onClick={handleMenuItemClick}>
             <Link to="/rooms/double-deluxe">Deluxe Room (ottoman)</Link>
           </li>
-          <li>
+          <li onClick={handleMenuItemClick}>
             <Link to="/rooms/family-deluxe">Family Deluxe Room (ottoman)</Link>
           </li>
-          <li>
+          <li onClick={handleMenuItemClick}>
             <Link to="/rooms/presidential-room">presedential Room (ottoman)</Link>
           </li>
           </span>
